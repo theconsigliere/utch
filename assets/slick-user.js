8eb6066ac72bbd9c5dccbc24c379446bcefc388d
Max Home <maxwell_kirwin@yahoo.co.uk>
2020-10-25 19:40:41 +0000

    all

---
+++ assets/slick-user.js
@@ -0,0 +1,27 @@
+$('.main-carousel').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  dots: true,
+  infinite: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
